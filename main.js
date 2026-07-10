d406b6b028450ad5f59e90d138f2a104607bbf2c
Sounak <sounak46@gmail.com>
2026-07-10 06:45:06 +0530

    remove debug openDevTools

--- main.js
+++ main.js
@@ -532,7 +532,6 @@ async function createWindow() {
 
   Menu.setApplicationMenu(null);
   await win.loadFile('index.html');
-  win.webContents.openDevTools();
 
   win.webContents.on('will-navigate', e => e.preventDefault());
   win.webContents.setWindowOpenHandler(() => ({ action: 'deny' }));
